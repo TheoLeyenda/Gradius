5eec1d5af5fd506afc05be3d3c8e20985dd7913c
Unknown <jquintanamartin@hotmail.com>
2017-10-10 15:32:30 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ export/windows/neko/haxe/lime/AssetData.hx
@@ -0,0 +1,54 @@
+package lime;
+
+
+import lime.utils.Assets;
+
+
+class AssetData {
+
+	private static var initialized:Bool = false;
+	
+	public static var library = new #if haxe3 Map <String, #else Hash <#end LibraryType> ();
+	public static var path = new #if haxe3 Map <String, #else Hash <#end String> ();
+	public static var type = new #if haxe3 Map <String, #else Hash <#end AssetType> ();	
+	
+	public static function initialize():Void {
+		
+		if (!initialized) {
+			
+			path.set ("assets/data/Gradius.oep", "assets/data/Gradius.oep");
+			type.set ("assets/data/Gradius.oep", Reflect.field (AssetType, "text".toUpperCase ()));
+			path.set ("assets/data/LEVEL.oel", "assets/data/LEVEL.oel");
+			type.set ("assets/data/LEVEL.oel", Reflect.field (AssetType, "text".toUpperCase ()));
+			path.set ("assets/data/lvlproto.oel", "assets/data/lvlproto.oel");
+			type.set ("assets/data/lvlproto.oel", Reflect.field (AssetType, "text".toUpperCase ()));
+			path.set ("assets/images/fideo.png", "assets/images/fideo.png");
+			type.set ("assets/images/fideo.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			path.set ("assets/images/PruevaScrolling.png", "assets/images/PruevaScrolling.png");
+			type.set ("assets/images/PruevaScrolling.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			path.set ("assets/images/Tiles.png", "assets/images/Tiles.png");
+			type.set ("assets/images/Tiles.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			path.set ("assets/images/wachin.png", "assets/images/wachin.png");
+			type.set ("assets/images/wachin.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			path.set ("flixel/sounds/beep.ogg", "flixel/sounds/beep.ogg");
+			type.set ("flixel/sounds/beep.ogg", Reflect.field (AssetType, "sound".toUpperCase ()));
+			path.set ("flixel/sounds/flixel.ogg", "flixel/sounds/flixel.ogg");
+			type.set ("flixel/sounds/flixel.ogg", Reflect.field (AssetType, "sound".toUpperCase ()));
+			path.set ("flixel/fonts/nokiafc22.ttf", "flixel/fonts/nokiafc22.ttf");
+			type.set ("flixel/fonts/nokiafc22.ttf", Reflect.field (AssetType, "font".toUpperCase ()));
+			path.set ("flixel/fonts/monsterrat.ttf", "flixel/fonts/monsterrat.ttf");
+			type.set ("flixel/fonts/monsterrat.ttf", Reflect.field (AssetType, "font".toUpperCase ()));
+			path.set ("flixel/images/ui/button.png", "flixel/images/ui/button.png");
+			type.set ("flixel/images/ui/button.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			path.set ("flixel/images/logo/default.png", "flixel/images/logo/default.png");
+			type.set ("flixel/images/logo/default.png", Reflect.field (AssetType, "image".toUpperCase ()));
+			
+			
+			initialized = true;
+			
+		} //!initialized
+		
+	} //initialize
+	
+	
+} //AssetData
